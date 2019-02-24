1f7998d3c1e30327f20bf01252c28a74e9d15bab
mongo <mongopa88@gmail.com>
2019-02-24 23:06:19 +0900

    add saveBtn

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,11 +5,12 @@
     <button @click="logout">ログアウト</button>
     <div>
       <div class="memoListWrapper">
-        <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" :data-selected="index == selectedIndex">
+        <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" :data-selected="index == selectedIndex">
           <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
         </div>
         <button class="addMemoBtn" @click="addMemo">メモの追加</button>
         <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+        <button class="saveMemoBtn" @click="saveMemo">メモの保存</button>
       </div>
       <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
       <div class="preview" v-html="preview()"></div>
@@ -31,6 +32,13 @@ export default {
       selectedIndex: 0
     };
   },
+  created:function(){
+    firebase.database().ref('memos/' + this.user.uid).once('value').then(result=>{
+      if(result.val()){
+        this.memos = result.val();
+      }
+    })
+  },
   methods: {
     logout: function() {
       firebase.auth().signOut();
@@ -49,12 +57,18 @@ export default {
     displayTitle: function(text) {
       return text.split(/\n/)[0];
     },
-    deleteMemo: function(){
+    deleteMemo: function() {
       this.memos.splice(this.selectedIndex, 1);
-      if(this.selectedIndex > 0){
+      if (this.selectedIndex > 0) {
         this.selectedIndex--;
       }
     },
+    saveMemo: function() {
+      firebase
+        .database()
+        .ref("memos/" + this.user.uid)
+        .set(this.memos);
+    }
   }
 };
 </script>
@@ -69,30 +83,27 @@ export default {
   box-sizing: border-box;
   text-align: left;
   border-bottom: 1px solid #000;
-  &:nth-child(even) {
-    background-color: #ccc;
-    &[data-selected="true"] {
-      background-color: #ccf;
-    }
+  &[data-selected="true"] {
+    background-color: #ccf;
   }
-  .memoTitle {
-    height: 1.5em;
-    margin: 0;
-    white-space: nowrap;
-    overflow: hidden;
-  }
-  .addMemoBtn {
-    margin-top: 20px;
-  }
-  .markdown {
-    float: left;
-    width: 40%;
-    height: 500px;
-  }
-  .preview {
-    float: left;
-    width: 40%;
-    text-align: left;
-  }
+}
+.memoTitle {
+  height: 1.5em;
+  margin: 0;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.addMemoBtn {
+  margin-top: 20px;
+}
+.markdown {
+  float: left;
+  width: 40%;
+  height: 500px;
+}
+.preview {
+  float: left;
+  width: 40%;
+  text-align: left;
 }
 </style>
