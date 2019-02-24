7b0d8c04dce0d3ca813d7d5b97f6993184f31231
mongo <mongopa88@gmail.com>
2019-02-24 22:32:09 +0900

    delete post

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,46 +1,98 @@
 <template>
   <div class="editor">
     <h1>エディター画面</h1>
-    <span>{{ user.displayName }}</span><br>
+    <span>{{ user.displayName }}</span>
     <button @click="logout">ログアウト</button>
-    <div class ="editorWrapper">
-        <textarea class="markdown" v-model="markdown"></textarea>
-        <div class="preview" v-html="preview()"></div>
+    <div>
+      <div class="memoListWrapper">
+        <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" :data-selected="index == selectedIndex">
+          <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+        </div>
+        <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+        <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+      </div>
+      <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
     </div>
   </div>
-</template>
-
+</template>
 <script>
-import marked from 'marked';
+import marked from "marked";
 export default {
-  name: 'editor',
-  props: ['user'],
-  data () {
+  name: "editor",
+  props: ["user"],
+  data() {
     return {
-        markdown: '',
-    }
+      memos: [
+        {
+          markdown: ""
+        }
+      ],
+      selectedIndex: 0
+    };
   },
-  methods:{
-      logout: function() {
-          firebase.auth().signOut();
-      },
-      preview:function() {
-          return marked(this.markdown);
+  methods: {
+    logout: function() {
+      firebase.auth().signOut();
+    },
+    addMemo: function() {
+      this.memos.push({
+        markdown: "無題のメモ"
+      });
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
+    preview: function() {
+      return marked(this.memos[this.selectedIndex].markdown);
+    },
+    displayTitle: function(text) {
+      return text.split(/\n/)[0];
+    },
+    deleteMemo: function(){
+      this.memos.splice(this.selectedIndex, 1);
+      if(this.selectedIndex > 0){
+        this.selectedIndex--;
       }
+    },
   }
-}
-</script>
+};
 </script>
 <style lang="scss" scoped>
-.editorWrapper{
-  display: flex;
+.memoListWrapper {
+  width: 19%;
+  float: left;
+  border-top: 1px solid #000;
 }
-.markdown {
-   width: 50%;
-   height: 500px;
-}
-.preview {
-    width: 50%;
+.memoList {
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: left;
+  border-bottom: 1px solid #000;
+  &:nth-child(even) {
+    background-color: #ccc;
+    &[data-selected="true"] {
+      background-color: #ccf;
+    }
+  }
+  .memoTitle {
+    height: 1.5em;
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+  .addMemoBtn {
+    margin-top: 20px;
+  }
+  .markdown {
+    float: left;
+    width: 40%;
+    height: 500px;
+  }
+  .preview {
+    float: left;
+    width: 40%;
     text-align: left;
+  }
 }
 </style>
